bbe6bdc6367a47ef7318751f75a953742a536b8c
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2024-07-28 10:37:12 +0300

    More documentation about the rewards of collecting a card

--- nft/contracts/IBuilderCard.sol
+++ nft/contracts/IBuilderCard.sol
@@ -3,6 +3,8 @@ pragma solidity 0.8.24;
 
 contract BuilderCardBase {
     uint256 public constant TOTAL_COLLECTION_FEE = 0.001 ether;
+    uint256 constant BUILDER_REWARD = 0.0005 ether;
+    uint256 public constant FIRST_COLLECTOR_REWARD = 0.0003 ether;
 
     constructor() {}
 }
@@ -24,6 +26,14 @@ interface IBuilderCard {
      * @notice Mints/Creates an instance of the Builder Card for the Builder with id `_id`. This instance is
      * attached to the `msg.sender`. Hence, the `msg.sender` is considered to be
      * the Collector of the Builder Card for the Builder given.
+     * Also, it rewards the parties involved and pays fee to the platform hosting
+     * the Builder Cards web application. Here is how reward is working:
+     * - Builder is getting `BuilderCardBase->BUILDER_REWARD`.
+     * - If the `msg.sender` is the first collector of the particular Builder Card,
+     * they are being rewarded `BuilderCardBase->FIRST_COLLECTOR_REWARD`.
+     * - Platform gets the rest, i.e. `TOTAL_COLLECTION_FEE - FIRST_COLLECTOR_REWARD(if they have been rewarded) - BUILDER_REWARD`.
+     * Note, that the `msg.value` of the transaction needs to be (at least) `TOTAL_COLLECTION_FEE`. Otherwise,
+     * the call will be reverted.
      * @dev
      * MUST revert, if the `msg.value` sent with this transaction is not enough to cover at least
      * for the `BuilderCardBase->TOTAL_COLLECTION_FEE`.
